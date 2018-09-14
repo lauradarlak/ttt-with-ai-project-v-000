a530fe84b3c0680fee6008483af120fe46ae1a8e
Laura Darlak <laura@polyporedesign.com>
2018-09-14 00:13:19 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -140,7 +140,7 @@ class Game
     case continue
     when "y" || "Y"
       puts "Restarting..."
-      Board.new
+      Board.reset!
       self.start_game
     when "N" || "n"
       puts "Thank for playing!"
